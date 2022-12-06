b049ade94d09bfda107cb058bc9be73d918770d4
Roy Chateau <chateau.royw@gmail.com>
2022-12-06 20:19:30 +0100

    Day 1: Part 2

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,4 +1,5 @@
 fn main() {
+    //Part 1
     let res = std::fs::read_to_string("input.txt");
     match res {
         Ok(content) => {
@@ -21,4 +22,33 @@ fn main() {
             println!("Error opening input.txt");
         }
     };
+
+    //Part 2
+    let content = std::fs::read_to_string("input.txt").expect("Error opening input.txt");
+    let mut top : Vec<u64> = vec![0; 3];
+    let mut cur : u64 = 0;
+    for l in content.lines() {
+        if l.len() == 0 {
+            let mut tmp : u64;
+            if cur > top[0] {
+                if cur > top[1] {
+                    if cur > top[2] {
+                        tmp = top[2];
+                        top[2] = cur;
+                        cur = tmp;
+                    }
+                    tmp = top[1];
+                    top[1] = cur;
+                    cur = tmp;
+                }
+                top[0] = cur;
+            }
+            cur = 0;
+        } else {
+            let cal : u64 = l.parse().unwrap();
+            cur = cur + cal;
+        }
+    }
+    println!("total = {}" , top[0] + top[1] + top[2]);
+
 }
