80be8f7f7e9dc3206abd5f0f64ae7eca521867ed
Roy Chateau <chateau.royw@gmail.com>
2022-12-21 21:13:04 +0100

    Day 3: Part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,30 +1,28 @@
-fn outer_join(first : &str, second : &str) -> char
+fn inner_join(first : &str, second : &str) -> String
 {
     let mut hseta = vec!(0; 256);
     let mut hsetb = vec!(0; 256);
-    let mut found : char = '+';
+    let mut found : String = "".to_string();
 
     for cc in first.chars() {
         let key : usize = cc as usize;
-        hseta[key] = hseta[key] + 1;
+        hseta[key] = 1;
     }
 
     for cc in second.chars() {
         let key : usize = cc as usize;
-        hsetb[key] = hsetb[key] + 1;
+        hsetb[key] = 1;
     }
 
     for i in 0..hseta.len() {
-        if (hseta[i] >= 1) && (hsetb[i] >= 1) {
-            found = char::from_u32(i.try_into().unwrap()).unwrap();
-            break;
+        if (hseta[i] == 1) && (hsetb[i] == 1) {
+            found.push(char::from_u32(i as u32).unwrap()); 
         }
     }
-
     found
 }
 
-fn to_number(c : char) -> u8 
+fn to_number(c : char) -> u64 
 {
     match c {
         'a' ..='z' => ((c as u8 - b'a')+1).into(),
@@ -37,20 +35,43 @@ fn process_part1(input : &str) -> u64 {
     input.lines()
     .map(|line| {
         let (first, second) = line.split_at(line.len()/2);
-        to_number(outer_join(first, second)) as u64
+
+        inner_join(first, second)
+        .chars()
+        .map(|a| to_number(a as char))
+        .sum::<u64>()
         })
     .sum()
 }
 
 fn process_part2(input : &str) -> u64 {
-    0
+    let mut lines = input.lines();
+    let mut total : u64 = 0;
+
+    loop {
+        let l1 = lines.next();
+        if let None = l1 {
+            break;
+        }
+
+        let l1 = l1.unwrap_or(""); 
+        let l2 = lines.next().unwrap_or("");
+        let l3 = lines.next().unwrap_or("");
+
+        total = total + 
+            inner_join(&inner_join(&l1, &l2), &l3)
+            .chars()
+            .map(|a| to_number(a))
+            .sum::<u64>();
+    }
+    total
 }
 
 fn main() {
     //part 1
     let content = std::fs::read_to_string("input.txt").expect("Error reading input.txt");
     println!("part1 = {}", process_part1(&content));
-    
+
     //part 2
     let content = std::fs::read_to_string("input.txt").expect("Error reading input.txt");
     println!("part2 = {}", process_part2(&content));
@@ -73,8 +94,8 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
         let result = process_part1(&INPUT);
         assert_eq!(result, 157);
 
-       // let result = process_part2(&INPUT);
-       // assert_eq!(result, 15);
+        let result = process_part2(&INPUT);
+        assert_eq!(result, 70);
     }
 }
 
